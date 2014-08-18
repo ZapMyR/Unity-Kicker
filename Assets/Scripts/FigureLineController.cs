b71b3bdbe0c77796bc2a4dbe788c1d66f96f2875
r-o-x <sebastian-meier@gmx.net>
2014-08-18 14:54:41 +0200

    Project added

---
+++ Assets/Scripts/FigureLineController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FigureLineController : MonoBehaviour {
+
+	public GameObject[] Figures;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject Goalkeeper;
+	public GameObject Defense;
+	public GameObject Midfield;
+	public GameObject Offense;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
